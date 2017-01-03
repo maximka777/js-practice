414845168819cee4c44c6b215513a3256029d4b3
Maxim Borovsky <maxim-borovsky@iMac-Senior-Node-6.local>
2017-01-03 17:17:13 +0300

    Fixed learnyounode/json-api-server.js

--- learnyounode/json-api-server.js
+++ learnyounode/json-api-server.js
@@ -2,18 +2,21 @@ var http = require('http');
 var url = require('url');
 
 var server = http.createServer(function(req, resp){
-	var iso = url.parse(req.url, true).query.iso;
+	var parsed = url.parse(req.url, true);
+	var iso = parsed.query.iso;
+	var path = parsed.pathname;
 	var date = new Date(Date.parse(iso));
 	resp.writeHead(200, {
 		'Content-Type': 'application/json'
 	});
-	var obj;
-	if(req.url.startsWith('/api/parsetime')){
+	var obj = {};
+	if(path == '/api/parsetime'){
 		obj = createTimeObject(date);
-	} else if(req.url.startsWith('/api/unixtime')){
+	} else if(path == '/api/unixtime'){
 		obj = createUnixTimeObject(date);
 	}
 	resp.write(JSON.stringify(obj));
+	resp.end('\n');
 });
 server.listen(Number(process.argv[2]));
 
