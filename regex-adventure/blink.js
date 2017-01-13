bb2e1b91c23ed06f22fcbde6637c3f0684cf1947
maximka777 <maksim.borovskij@yandex.ru>
2017-01-13 16:54:13 +0300

    learnyoubash is began, learnyoureact, pattern-lab-workshop, planetproto, last task from regex-adventure

--- regex-adventure/blink.js
+++ regex-adventure/blink.js
@@ -1,4 +1,10 @@
+var marked = require('marked');
+
 module.exports = function (str) {
-	var arr = /@@([\d\w]+)@@/g.exec(str);
-	return str.replace(new RegExp(arr[0]), `<blink>${arr[1]}</blink>`);
+	str = marked(str);
+	var arr;
+	while(arr = /@@([\d\w<>/]+)@@/g.exec(str)){
+		str = str.replace(new RegExp(arr[0]), `<blink>${arr[1]}</blink>`);
+	}
+	return str;
 }
